c8dfe9728c2794893f928e55eccda2f895e3a55f
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2021-10-12 18:09:13 -0300

    Aula 13

--- ambiente.js
+++ ambiente.js
@@ -1,6 +1,22 @@
-console.log('Vai comecar...')
-for (var c = 1;c <= 10;c++) {
-    console.log(c)
-}
-console.log('FIM')
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
 
+/* var c = 1
+while (c <= 500) {
+    console.log(`Passo ${c}`)
+    c++
+} /*
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
