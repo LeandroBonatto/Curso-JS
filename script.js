0722ad1e2e3ac6a3394145a479e331df8b93531c
Leandro Bonatto <84813263+LeandroBonatto@users.noreply.github.com>
2021-10-12 18:15:51 -0300

    Aula 14 - Ex 017

--- script.js
+++ script.js
@@ -1,34 +1,19 @@
-function contar() {
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
-
-
-if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-    res.innerHTML = 'Impossível contar!'
-    window.alert('[ERRO] Faltam dados!')
-} else {
-    res.innerHTML = 'Contando: '
-    let i = Number(ini.value)
-    let f = Number(fim.value)
-    let p = Number(passo.value)
-    if (p <= 0) {
-        window.alert('Passo inválido!  Considerando PASSO 1')
-        p = 1
-    }
-    if (i < f) {
-        // Contagem crescente
-        for(let c = i; c <= f; c += p) {
-            res.innerHTML += `${c} \u{1F449}`
-            }
-            res.innerHTML += `\u{1F3C1}`
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
     } else {
-        // Contagem regressiva
-        for (let c = i; c >= f; c -= p) {
-            res.innerHTML += `${c} \u{1F449}`
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
         }
     }
-    res.innerHTML += `${c} \u{1F449}`
-    }
 }
+
